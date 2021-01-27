8b0b4104b4c124f51ee9a5cbc6aeda476d06c01c
kenji <kenjix12x22@gmail.com>
2021-01-28 00:22:26 +0900

    apply grid -row

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: {
+        gridRow: '2',
+    }
+});
 
 const MessageInputField = () => {
-    return <div>MessageList</div>;
+    const classes = useStyles();
+    return <div className={classes.root}>MessageInputFeild</div>;
 }
 
 export default MessageInputField;
