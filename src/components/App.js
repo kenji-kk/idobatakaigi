724dd4cb20e5c64eaab9a2e9d38047c7313b3c15
kenji <kenjix12x22@gmail.com>
2021-01-27 20:12:59 +0900

    create setName

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 import SignIn from './SingIn';
 
 export default () => {
-  return <SignIn />;
+  const {name, setName} = useState('');
+  console.log({name});
+
+  return <SignIn setName={setName} />;
 };
