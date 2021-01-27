97a41ef4e8d8b90fae7be6b3b4b902c4c6fbe0d7
kenji <kenjix12x22@gmail.com>
2021-01-27 22:38:04 +0900

    save name in japanese

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React,{useState} from 'react';
 
+import Main from './Main'
 import SignIn from './SingIn';
 
 export default () => {
   const [name, setName] = useState('');
   console.log({name});
-
+  
+  if (name === ''){
   return <SignIn setName={setName} />;
+  }else{
+    return <Main name={name} />;
+  }
 };
--- src/components/SingIn.js
+++ src/components/SingIn.js
@@ -46,6 +46,7 @@ export default function SignIn({ setName }) {
   const classes = useStyles();
   const [disabled, setDisabled] = useState(true);
   const [string, setString] = useState('');
+  const [isComposed, setIsComposed] = useState(false);
 
   useEffect(() => {
     const disabled = string === ''
@@ -71,12 +72,15 @@ export default function SignIn({ setName }) {
             autoFocus
             onChange={(e) => setString(e.target.value)}
             onKeyDown={(e) => {
-              console.log({key: e.key});
+              if (isComposed) return;
+              
               if (e.key === 'Enter'){
                 setName(e.target.value);
                 e.preventDefault();
               }
             }}
+            onCompositionStart={() => setIsComposed(true)}
+            onCompositionEnd={() => setIsComposed(false)}
           />
           <Button
             type="button"
